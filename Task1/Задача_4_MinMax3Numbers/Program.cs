0b8d3f833dc8bb7312dfb1cd66354d9a9809d960
Ivan <vanrock00@yandex.ru>
2023-03-12 23:48:56 +1000

    Задача 4 выполнена

---
+++ Task1/Задача_4_MinMax3Numbers/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.Clear();
+Console.Write("Введите первое число: ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int B = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int C = Convert.ToInt32(Console.ReadLine());
+
+int max = A;
+
+if (B > max)
+    {
+        max = B;
+    }
+if (C > max)
+    {
+        max = C;
+    }
+else
+    {
+        Console.WriteLine("Все числа равны");
+    }
+if (A != B || B != C || A != C)
+    {
+        Console.WriteLine($"Максимальное число : {max}");
+    }    
