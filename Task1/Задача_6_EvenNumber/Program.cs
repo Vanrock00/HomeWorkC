9f78219c152851d222c52cb725fdbb05db051208
Ivan <vanrock00@yandex.ru>
2023-03-13 00:00:51 +1000

    Выполнена 6 задача

---
+++ Task1/Задача_6_EvenNumber/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int A = Convert.ToInt32(Console.ReadLine());
+
+if (A % 2 == 0)
+    {
+        Console.WriteLine("Число четное");
+    }
+else
+    {
+        Console.WriteLine("Число не четное");
+    }
