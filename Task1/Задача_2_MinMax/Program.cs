b2989f555a5fe24d3b28684f491a6779b45a7515
Ivan <vanrock00@yandex.ru>
2023-03-12 23:23:10 +1000

    Задача 2 выполнена

---
+++ Task1/Задача_2_MinMax/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+Console.Write("Введите первое число: ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int B = Convert.ToInt32(Console.ReadLine());
+
+if (A > B)
+    {
+        Console.WriteLine("Первое число больше");
+    }
+else if (A < B)
+    {
+        Console.WriteLine("Второе число больше");
+    }
+else 
+    {
+        Console.WriteLine("Числа равны");
+    }
