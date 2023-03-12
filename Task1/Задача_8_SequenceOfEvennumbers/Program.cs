2484c08a5870f7dc177a8c0da398c015e0fa9fab
Ivan <vanrock00@yandex.ru>
2023-03-13 00:12:33 +1000

    Задача 8 выполнена

---
+++ Task1/Задача_8_SequenceOfEvennumbers/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.Write("Введите конечное число прогрессии: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+for(int i = 1; i < N; i++)
+    {
+        if(i % 2 == 0)
+            {
+                Console.Write($"{i} ");
+            }
+    }
