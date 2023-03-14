bdebc2d6eb931e44a005c6c803b986b7f61d8d0e
Ivan <vanrock00@yandex.ru>
2023-03-14 22:31:27 +1000

    Задача 15 выполнена

---
+++ Task2/Задача_15/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+Console.WriteLine("Введите цифру обозначающую день недели: ");
+int n = Convert.ToInt32(Console.ReadLine());
+if(n < 1 || n > 7)
+{
+    Console.Write("Это не день недели, попробуйте еще раз: ");
+    n = Convert.ToInt32(Console.ReadLine());
+}
+else
+{
+    if(n >= 6)
+        Console.WriteLine("Да");
+    else
+        Console.WriteLine("Нет");
+}
