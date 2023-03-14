632f20beee2effdff8b9cce5d741fa21ed196fdc
Ivan <vanrock00@yandex.ru>
2023-03-14 22:22:56 +1000

    Задача 13 выполнена

---
+++ Task2/Задача_13/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int n1 = n;
+if (n < 100)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+else
+{
+    while (n1 > 999)
+    {
+        n1 = n1 / 10;
+    }
+
+    Console.WriteLine($"Третья цифра числа {n} - {n1 % 10}");
+}
