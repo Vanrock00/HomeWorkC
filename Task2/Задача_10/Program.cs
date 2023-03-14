59176a1d8dddff2181deb00b7904f373f84edb3e
Ivan <vanrock00@yandex.ru>
2023-03-14 21:30:19 +1000

    Задача 10 выполнена

---
+++ Task2/Задача_10/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Clear();
+Console.Write("Введите трехзначное число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int n2 = (n / 10) % 10;
+Console.WriteLine($"Вторая цифра числа {n} - {n2}");
