20b9f26e0ca58e672f55224911784782fa63c0d5
Ivan <vanrock00@yandex.ru>
2023-03-14 23:28:58 +1000

    Дополнительная задача 1 выполнена

---
+++ Task2/ДопЗадача_1/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+Console.WriteLine("Введите последовательность чисел, для окончания ввода введите 0");
+int n = Convert.ToInt32(Console.ReadLine());
+int max = 0;
+int SecondMax = 0;
+do
+{
+
+    if (n > max)
+    {
+        SecondMax = max;
+        max = n;
+    }
+
+    n = Convert.ToInt32(Console.ReadLine());
+}
+while(n != 0);
+Console.WriteLine($"Первое максимальное число: {max}");
+Console.WriteLine($"Второе максимальное число: {SecondMax}");
