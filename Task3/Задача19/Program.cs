e762f4f1efbf7436bcd15826ad66cf28c6a0bc2d
Ivan <vanrock00@yandex.ru>
2023-03-16 23:57:59 +1000

    Задача19 Выполнена

---
+++ Task3/Задача19/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+Console.WriteLine("Введите пятизначное число N: ");
+string n = Console.ReadLine()!;
+int n1 = Convert.ToInt32(n);
+while (n1 < 0 || n1 > 99999)
+{
+    Console.WriteLine("Вы ошиблись! \nВведите пятизначное число N: ");
+    n = Console.ReadLine()!;
+}
+if(n[0] == n[4] && n[1] == n[3])
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
