b4f63b8c5d6a7b7760ae4f0e0633bb092b520eb5
Ivan <vanrock00@yandex.ru>
2023-03-23 23:15:14 +1000

    Задача 25 выполнена

---
+++ Task4/Задача25/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.Write("Введите основание: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите степень: ");
+int b = Convert.ToInt32(Console.ReadLine());
+int result = 1;
+for(int i = 0; i < b; i++)
+{
+    result *= a; 
+}
+Console.WriteLine(result);
