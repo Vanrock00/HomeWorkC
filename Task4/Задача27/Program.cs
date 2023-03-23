34f272018e6e506009653ff49faf63c6d7b0f09f
Ivan <vanrock00@yandex.ru>
2023-03-23 23:39:08 +1000

    Задача 27 выполнена

---
+++ Task4/Задача27/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Clear();
+int[] array = new int[8];
+for(int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(1, 40);
+}
+Console.WriteLine($"[{string.Join(", ", array)}]");
