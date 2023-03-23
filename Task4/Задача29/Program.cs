3fb6b9e7edd8b54d5e7d9dbf3f6a8edd23c85573
Ivan <vanrock00@yandex.ru>
2023-03-23 23:24:17 +1000

    Задача 29 выполнена

---
+++ Task4/Задача29/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+string n = Console.ReadLine()!;
+int sum = 0;
+for(int i = 0; i < n.Length; i++)
+{
+    sum += Convert.ToInt32(Convert.ToString(n[i]));
+}
+Console.WriteLine(sum);
