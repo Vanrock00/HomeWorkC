2f69f235127c051954b0742bf27f71cb4418e888
Ivan <vanrock00@yandex.ru>
2023-03-27 23:53:44 +1000

    Задача 38 выполнена

---
+++ Task5/Задача38/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear();
+Console.Write("Введите количество элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+double[] array = new double[n];
+double max = 0;
+double min = 99999999;
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = Math.Round(new Random().NextDouble() * 100, 2);
+    if(array[i] > max)
+    {
+        max = array[i];
+    }
+    else if(array[i] < min)
+    {
+        min = array[i];
+    }
+}
+Console.WriteLine($"[{string.Join(" ", array)}]");
+Console.WriteLine(Math.Round(max - min, 2));
