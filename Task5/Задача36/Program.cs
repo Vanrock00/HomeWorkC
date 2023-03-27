8f413c8a5e74dba5b44c1d01bbb2d42d162aa567
Ivan <vanrock00@yandex.ru>
2023-03-27 23:35:11 +1000

    Задача 36 выполнена

---
+++ Task5/Задача36/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+Console.Write("Введите количество элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+int[] array = new int[n];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(-100, 101);
+    if (i % 2 != 0)
+    {
+        sum += array[i];
+    }
+}
+Console.WriteLine($"[{string.Join(", ", array)}]");
+Console.WriteLine(sum);
