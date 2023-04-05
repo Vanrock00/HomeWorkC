af0ac7b8920d37f87eed143da2c5e0f47e893fd0
Ivan <vanrock00@yandex.ru>
2023-04-05 23:34:59 +1000

    Задача 43 выполнена

---
+++ Task6/Задача43/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+Console.Write("Введите количество чисел: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+int count = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write($"Введите {i+1} элемент массива: ");
+    array[i] = Convert.ToInt32(Console.ReadLine());
+    if(array[i] > 0)
+    {
+        count++;
+    }
+}
+Console.WriteLine($"Заданный массив: [{string.Join(", ", array)}]");
+Console.WriteLine($"Колличество элементов >0: {count}");
+
