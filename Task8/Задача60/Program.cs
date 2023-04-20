bc7a2f1df8302bf70d1324665edba9fb1f2b9907
Ivan <vanrock00@yandex.ru>
2023-04-21 06:22:45 +1000

    Задача 60 трехмерный массив выполнена

---
+++ Task8/Задача60/Program.cs
@@ -0,0 +1,32 @@
+﻿void InputMatrix(int[,,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for(int k = 0; k < matrix.GetLength(2); k++)
+            matrix[i,j,k] = new Random().Next(1, 11);
+        }
+    }
+}
+
+
+void PrintMatrix(int[,,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {   
+            for (int k = 0; k < matrix.GetLength(2); k++) 
+                Console.Write($"{matrix[i,j,k]}({i},{j},{k}) \t");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.Clear();
+Console.Write("Введите размерность матриц: ");
+int[] size = Console.ReadLine()!.Split().Select(x => int.Parse(x)).ToArray();
+int[,,] a = new int[size[0], size[1],size[2]];
+InputMatrix(a);
+PrintMatrix(a);
