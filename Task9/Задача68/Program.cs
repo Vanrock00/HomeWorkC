bdb89de1f422c8a9712b3222c9f4c7c8433f4e8e
Ivan <vanrock00@yandex.ru>
2023-04-22 01:22:05 +1000

    Задача 68 выполнена

---
+++ Task9/Задача68/Program.cs
@@ -0,0 +1,16 @@
+﻿int ack(int a, int b)
+{
+    if (a == 0)
+        return b + 1;
+    else if(b == 0)
+        return ack(a-1,1);
+    return ack(a-1, ack(a, b-1));
+}
+
+
+Console.Clear();
+Console.Write("Введите число M: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число N: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(ack(a,b));
