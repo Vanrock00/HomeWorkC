992faed0c324c717d5bdb3b768366ae934bf176b
Ivan <vanrock00@yandex.ru>
2023-04-22 01:06:17 +1000

    Задача 66 выполнена

---
+++ Task9/Задача66/Program.cs
@@ -0,0 +1,14 @@
+﻿int f(int m, int n)
+{
+    if (n == m)
+        return m;
+    return f(m, n - 1) + n;
+}
+
+
+Console.Clear();
+Console.Write("Введите число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(f(m,n));
